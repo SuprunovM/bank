182f2fee43d2bc1b801e65b9ec7eebd4f64f60da
SuprunovM <144795321+SuprunovM@users.noreply.github.com>
2024-04-08 11:44:58 +0300

    Update dfdf.cpp

--- dfdf/dfdf.cpp
+++ dfdf/dfdf.cpp
@@ -1,64 +1,52 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
 #define random(min,max) (min + rand() % (max - min + 1))
 using namespace std;
-const char NUMBERS[] = { '0','1','2','3','4','5','6','7','8','9' };
-const string firstNameArr[] = { "" };
-const string lastNameArr[] = { "" };
+
 class Client;
 class Bank;
 
-
 class Client {
     friend class Bank;
+
+
 private:
     static int lastId;
-
+    int id;
     string firstName;
     string lastName;
-    int age;
+    unsigned short age;
     string bankAccount;
     double balance = 0;
-    int id;
 
-public:
-    Client() {}
+    Client() {};
 
-    Client(string firstName, string lastName, int age) {
-        this->firstName = firstName;
-        this->lastName = lastName;
-        this->age = age;
+    Client(string firstName, string lastName, unsigned short age, string bankAccount) {
         this->id = lastId++;
-
-    }
-
-    void setFirstName(string firstName) {
         this->firstName = firstName;
-    }
-
-    void setLastName(string lastName) {
         this->lastName = lastName;
-    }
-
-    void setAge(int age) {
         this->age = age;
+        this->bankAccount = bankAccount;
     }
 
-    void showInfo() {
-        cout << "First name: " << this->firstName << endl;
-        cout << "Last name: " << this->lastName << endl;
-        cout << "Age: " << this->age << endl;
-        cout << "ID: " << this->id << endl;
-        cout << "Balance: " << this->balance << endl;
-        cout << "\n\n";
+    void setFirstName(string fName) {
+        this->firstName = fName;
     }
 
     string getFirstName() {
         return this->firstName;
     }
 
+    void setLastName(string lName) {
+        this->lastName = lName;
+    }
+
     string getLastName() {
         return this->lastName;
+    }
+
+    void setAge(int age) {
+        this->age = age;
     }
 
     int getAge() {
@@ -69,63 +57,117 @@ public:
         return this->bankAccount;
     }
 
-    double  getBalance() {
+    void setBalance(double balance) {
+        this->balance = balance;
+    }
+
+    double getBalance() {
         return this->balance;
     }
 
-};
-
-
-class Bank {
-private:
-    Client** clients = new Client * [0];
-public:
-    string name;
-    int size = 0;
-    Bank() {
-
+    void showInfo() {
+        cout << "ID: " << this->id << endl;
+        cout << "First name: " << this->firstName << endl;
+        cout << "Last name: " << this->lastName << endl;
+        cout << "Age: " << this->age << endl;
+        cout << "Bank account: " << this->bankAccount << endl;
+        //cout << "Balance" << this->balance << endl;
+        cout << "\n\n";
     }
 
-    Bank(string bankName) {
-        this->name = bankName;
-    }
-
-    bool checkBankAcc(string newBankAcc, string** clients) {
-        for (int i = 0; i < size; i++) {
-            if (newBankAcc == this->clients[i]->getBankAccount()) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    void createBankAccount() {
-        bool flag = true;
-        string bankAccount = "";
-
-        while (flag) {
-            for (int i = 0; i < 8; i++)
-            {
-                bankAccount += NUMBERS[random(0, 9)];
-
-            }
-        }
-    }
 };
 
 int Client::lastId = 1;
 
+class Bank {
+private:
+    string name;
+    Client* clients = new Client[0];
+    int size = 0;
+public:
+    Bank(string name) {
+        this->name = name;
+    }
+
+    ~Bank() {
+        delete[] clients;
+    }
+    
+    void createClient(string firstName,
+        string lastName,
+        unsigned short age) {
+        Client* buf = new Client[size + 1];
+        for (int i = 0; i < size; i++)
+        {
+            buf[i] = clients[i];
+        }
+        delete[] clients;
+        clients = buf;
+        clients[size] = Client(firstName, lastName, age, createBankAccount());
+        size++;
+    }
+
+    void ShowClients() {
+        for (int i = 0; i < size; i++) {
+            clients[i].showInfo();
+        }
+    }
+
+    void showInfo(int clientID) {
+        for (int i = 0; i < this->size; i++) {
+            if (this->clients[i].id == clientID) {
+                this->clients[i].showInfo();
+                return;
+            }
+        }
+    }
+    void removeClient(int clientID) {
+        bool flag = false;
+        for (int i = 0; i < this->size; i++) {
+            if (this->clients[i].id == clientID) {
+                flag = true;
+            }
+        }
+        if (!flag);
+        Client* buf = new Client[this->size - 1];
+        for (int i = 0, k = 0; i < this->size; i++) {
+            if (this->clients[i].id != clientID) {
+                buf[k++] = this->clients[i];
+            }
+        }
+        this->size++;
+        delete[]this->clients;
+        this->clients = buf;
+    }
+
+private:
+
+    string createBankAccount() {
+        string bankAccount = "";
+        do {
+            bankAccount += to_string(random(1000, 9999));
+            bankAccount += to_string(random(1000, 9999));
+        } while (checkBankAccount(bankAccount));
+        return bankAccount;
+
+    }
+
+    bool checkBankAccount(string bankAccount) {
+        for (int i = 0; i < size; i++) {
+            if (clients[i].getBankAccount() == bankAccount) return true;
+        }
+        return false;
+    }
+
+};
+
+
 int main()
 {
-    srand(time(NULL));
-
-    Client* cl1 = new Client(firstNameArr[random(0, 9)], lastNameArr[random(0, 9)], random(18, 70));
-    cl1->showInfo();
-
-    Client* cl2 = new Client(firstNameArr[random(0, 9)], lastNameArr[random(0, 9)], random(18, 70));
-    cl2->showInfo();
-
-    Client* cl3 = new Client(firstNameArr[random(0, 9)], lastNameArr[random(0, 9)], random(18, 70));
-    cl3->showInfo();
-
+    Bank bank("Tinkoff");
+    for (int i = 0; i < 200; i++)
+    {
+    bank.createClient("Ben", "Tom", 18);
+    }
+    bank.ShowClients();
 }
